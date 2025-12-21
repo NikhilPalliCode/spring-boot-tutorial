1569275c6d034384adf830f2cd0718fc1793132a
NikhilPalliCode <nikhilpalle1997@gmail.com>
2025-12-21 11:19:48 -0600

    Spring starter web

--- src/main/java/com/nutorials/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/nutorials/spring_demo/SpringDemoApplication.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringDemoApplication {
 
@@ -13,6 +17,10 @@ public class SpringDemoApplication {
 
 		Dev dev = context.getBean(Dev.class);
 		dev.build();
+	}
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hello, Welcome to the HomePage";
 	}
 
 }
